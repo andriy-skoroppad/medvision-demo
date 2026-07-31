df287c2aa1b0e0f02069859866701740eeed71c3
Andriy Skoropad <andry.skoropad@gmail.com>
2026-07-31 17:06:11 +0300

    release-v.0.0.7

---
+++ chunk-WJJ23BHY.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"",loadComponent:()=>import("./chunk-JYST4OMR.js").then(o=>o.BookingSettingsPageComponent)},{path:"journal",loadComponent:()=>import("./chunk-GG2UN3CY.js").then(o=>o.BookingJournalPageComponent)}];export{t as ONLINE_BOOKING_ROUTES};
