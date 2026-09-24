288be00588c576183eb61d4cbf4fcdc5364835c6
Andriy Skoropad <andry.skoropad@gmail.com>
2026-09-25 01:56:17 +0300

    release-v.0.0.32

---
+++ chunk-D2M25LEY.js
@@ -0,0 +1,177 @@
+;(new Function(atob("LyogTWVkIFZpc2lvbiBkb21haW4gZ3VhcmQg4oCUIHBheWxvYWQuDQogKg0KICogTk9UIGltcG9ydGVkIGJ5IGFwcCBjb2RlLiBgc2NyaXB0cy9pbmplY3QtZ3VhcmQubWpzYCByZWFkcyB0aGlzIGZpbGUsIHJlcGxhY2VzDQogKiB0aGUgQUxMT1dFRC1oYXNoZXMgdG9rZW4gd2l0aCB0aGUgU0hBLTI1NiBoYXNoZXMgb2YgdGhlIGFsbG93ZWQgaG9zdG5hbWVzLA0KICogYmFzZTY0LWVuY29kZXMgdGhlIHdob2xlIHRoaW5nIGFuZCBpbmplY3RzIGEgYm9vdHN0cmFwIDxzY3JpcHQ+IGludG8gdGhlIGJ1aWx0DQogKiBpbmRleC5odG1sIG9mIGVhY2ggYXBwLiBUaGUgYWxsb3dlZCBkb21haW4gdGhlcmVmb3JlIG5ldmVyIHNoaXBzIGFzIHBsYWludGV4dCDigJQNCiAqIG9ubHkgaXRzIGhhc2ggZG9lcywgc28gZ3JlcHBpbmcgdGhlIGRlcGxveWVkIGJ1bmRsZSBmb3IgdGhlIGRvbWFpbiBmaW5kcyBub3RoaW5nLg0KICoNCiAqIE9uIGEgZm9yZWlnbiBob3N0IHRoZSBwYWdlIGlzIHNpbGVudGx5IGJyb2tlbiBhZnRlciBhIHJhbmRvbWlzZWQgZGVsYXksIHdoaWNoDQogKiBtYWtlcyB0aGUgdHJpZ2dlciBoYXJkIHRvIGNvcnJlbGF0ZSB3aXRoIHRoZSBsb2FkIGFuZCBoYXJkIHRvIGxvY2F0ZS4NCiAqLw0KKGZ1bmN0aW9uICgpIHsNCiAgLy8gSW5qZWN0ZWQgaW50byBpbmRleC5odG1sIEFORCBldmVyeSBidWlsdCAuanMgY2h1bmssIHNvIGl0IHJ1bnMgb24gbG9hZCBubw0KICAvLyBtYXR0ZXIgd2hpY2ggZmlsZSB0aGUgYnJvd3NlciByZWFjaGVzIGZpcnN0LiBUaGlzIGZsYWcgbWFrZXMgdGhlIGFjdHVhbA0KICAvLyBjaGVjayBydW4gb25jZSBwZXIgcGFnZSBpbnN0ZWFkIG9mIG9uY2UgcGVyIGNodW5rLg0KICB0cnkgew0KICAgIGlmICh3aW5kb3cuX19tdmcpIHJldHVybjsNCiAgICB3aW5kb3cuX19tdmcgPSAxOw0KICB9IGNhdGNoIChlKSB7fQ0KDQogIHZhciBBTExPV0VEID0gWyJkZTM2NWNkYzljZjg2OTgxZWYyZDg0OWU0N2YzYTk5YWY1ZGRjZDkzZGM0NWFlM2QzYmZmZjM2ZTU2NmMwMjA1IiwiNDk5NjBkZTU4ODBlOGM2ODc0MzQxNzBmNjQ3NjYwNWI4ZmU0YWViOWEyODYzMmM3OTk1Y2YzYmE4MzFkOTc2MyIsIjEyY2ExN2I0OWFmMjI4OTQzNmYzMDNlMDE2NjAzMGEyMWU1MjVkMjY2ZTIwOTI2NzQzMzgwMWE4ZmQ0MDcxYTAiXTsNCg0KICBmdW5jdGlvbiB3aXBlKCkgew0KICAgIHRyeSB7DQogICAgICBpZiAod2luZG93LnN0b3ApIHdpbmRvdy5zdG9wKCk7DQogICAgfSBjYXRjaCAoZSkge30NCiAgICB0cnkgew0KICAgICAgZG9jdW1lbnQuZG9jdW1lbnRFbGVtZW50LmlubmVySFRNTCA9ICcnOw0KICAgIH0gY2F0Y2ggKGUpIHt9DQogIH0NCg0KICBmdW5jdGlvbiB0cmlwKCkgew0KICAgIC8vIDPigJMxNXM6IGxvbmcgZW5vdWdoIHRoYXQgdGhlIGJyZWFrIGxvb2tzIHVucmVsYXRlZCB0byB0aGUgZG9tYWluIGNoZWNrLg0KICAgIHZhciBkZWxheSA9IDMwMDAgKyAoKE1hdGgucmFuZG9tKCkgKiAxMjAwMCkgfCAwKTsNCiAgICBzZXRUaW1lb3V0KGZ1bmN0aW9uICgpIHsNCiAgICAgIHdpcGUoKTsNCiAgICAgIC8vIEtlZXAgaXQgYnJva2VuIGV2ZW4gaWYgc29tZXRoaW5nIHRyaWVzIHRvIHJlLXJlbmRlci4NCiAgICAgIHRyeSB7DQogICAgICAgIHNldEludGVydmFsKHdpcGUsIDUwMCk7DQogICAgICB9IGNhdGNoIChlKSB7fQ0KICAgIH0sIGRlbGF5KTsNCiAgfQ0KDQogIHRyeSB7DQogICAgdmFyIGhvc3QgPSAobG9jYXRpb24uaG9zdG5hbWUgfHwgJycpLnRvTG93ZXJDYXNlKCk7DQogICAgdmFyIGJ5dGVzID0gbmV3IFRleHRFbmNvZGVyKCkuZW5jb2RlKGhvc3QpOw0KICAgIC8vIGNyeXB0by5zdWJ0bGUgbmVlZHMgYSBzZWN1cmUgY29udGV4dCDigJQgdHJ1ZSBvbiBodHRwcyBhbmQgb24gbG9jYWxob3N0Lg0KICAgIGNyeXB0by5zdWJ0bGUNCiAgICAgIC5kaWdlc3QoJ1NIQS0yNTYnLCBieXRlcykNCiAgICAgIC50aGVuKGZ1bmN0aW9uIChidWYpIHsNCiAgICAgICAgdmFyIGhleCA9IEFycmF5LnByb3RvdHlwZS5tYXANCiAgICAgICAgICAuY2FsbChuZXcgVWludDhBcnJheShidWYpLCBmdW5jdGlvbiAobikgew0KICAgICAgICAgICAgcmV0dXJuIChuIDwgMTYgPyAnMCcgOiAnJykgKyBuLnRvU3RyaW5nKDE2KTsNCiAgICAgICAgICB9KQ0KICAgICAgICAgIC5qb2luKCcnKTsNCiAgICAgICAgaWYgKEFMTE9XRUQuaW5kZXhPZihoZXgpIDwgMCkgdHJpcCgpOw0KICAgICAgfSkNCiAgICAgIFsnY2F0Y2gnXShmdW5jdGlvbiAoKSB7fSk7DQogIH0gY2F0Y2ggKGUpIHt9DQp9KSgpOw0K")))();
+import{f as j}from"./chunk-WUSLBJ3A.js";import{f as F}from"./chunk-5JHS22UP.js";import{g as L}from"./chunk-QN2SSMKT.js";import{$ as C,$b as T,Ca as w,Cb as s,Db as b,Eb as k,Jb as m,Kb as a,Kc as z,Lb as o,Mb as d,Sb as u,Tc as f,Z as x,Zb as l,_a as i,_b as D,ba as v,bc as E,cc as O,cd as h,dc as W,hc as R,ic as _,ja as y,jc as A,ka as S,kc as N,lc as B,mc as I,ob as g,pb as M,ub as P}from"./chunk-YKMIAOWI.js";var $=["determinateSpinner"];function K(r,p){if(r&1&&(y(),a(0,"svg",11),d(1,"circle",12),o()),r&2){let e=l();s("viewBox",e._viewBox()),i(),_("stroke-dasharray",e._strokeCircumference(),"px")("stroke-dashoffset",e._strokeCircumference()/2,"px")("stroke-width",e._circleStrokeWidth(),"%"),s("r",e._circleRadius())}}var U=new C("mat-progress-spinner-default-options",{providedIn:"root",factory:()=>({diameter:V})}),V=100,Z=10,Q=(()=>{class r{_elementRef=v(w);_noopAnimations;get color(){return this._color||this._defaultColor}set color(e){this._color=e}_color;_defaultColor="primary";_determinateCircle;constructor(){let e=v(U),t=j(),n=this._elementRef.nativeElement;this._noopAnimations=t==="di-disabled"&&!!e&&!e._forceAnimations,this.mode=n.nodeName.toLowerCase()==="mat-spinner"?"indeterminate":"determinate",!this._noopAnimations&&t==="reduced-motion"&&n.classList.add("mat-progress-spinner-reduced-motion"),e&&(e.color&&(this.color=this._defaultColor=e.color),e.diameter&&(this.diameter=e.diameter),e.strokeWidth&&(this.strokeWidth=e.strokeWidth))}mode;get value(){return this.mode==="determinate"?this._value:0}set value(e){this._value=Math.max(0,Math.min(100,e||0))}_value=0;get diameter(){return this._diameter}set diameter(e){this._diameter=e||0}_diameter=V;get strokeWidth(){return this._strokeWidth??this.diameter/10}set strokeWidth(e){this._strokeWidth=e||0}_strokeWidth;_circleRadius(){return(this.diameter-Z)/2}_viewBox(){let e=this._circleRadius()*2+this.strokeWidth;return`0 0 ${e} ${e}`}_strokeCircumference(){return 2*Math.PI*this._circleRadius()}_strokeDashOffset(){return this.mode==="determinate"?this._strokeCircumference()*(100-this._value)/100:null}_circleStrokeWidth(){return this.strokeWidth/this.diameter*100}static \u0275fac=function(t){return new(t||r)};static \u0275cmp=g({type:r,selectors:[["mat-progress-spinner"],["mat-spinner"]],viewQuery:function(t,n){if(t&1&&E($,5),t&2){let c;O(c=W())&&(n._determinateCircle=c.first)}},hostAttrs:["role","progressbar","tabindex","-1",1,"mat-mdc-progress-spinner","mdc-circular-progress"],hostVars:18,hostBindings:function(t,n){t&2&&(s("aria-valuemin",0)("aria-valuemax",100)("aria-valuenow",n.mode==="determinate"?n.value:null)("mode",n.mode),N("mat-"+n.color),_("width",n.diameter,"px")("height",n.diameter,"px")("--mat-progress-spinner-size",n.diameter+"px")("--mat-progress-spinner-active-indicator-width",n.diameter+"px"),A("_mat-animation-noopable",n._noopAnimations)("mdc-circular-progress--indeterminate",n.mode==="indeterminate"))},inputs:{color:"color",mode:"mode",value:[2,"value","value",h],diameter:[2,"diameter","diameter",h],strokeWidth:[2,"strokeWidth","strokeWidth",h]},exportAs:["matProgressSpinner"],decls:14,vars:11,consts:[["circle",""],["determinateSpinner",""],["aria-hidden","true",1,"mdc-circular-progress__determinate-container"],["xmlns","http://www.w3.org/2000/svg","focusable","false",1,"mdc-circular-progress__determinate-circle-graphic"],["cx","50%","cy","50%",1,"mdc-circular-progress__determinate-circle"],["aria-hidden","true",1,"mdc-circular-progress__indeterminate-container"],[1,"mdc-circular-progress__spinner-layer"],[1,"mdc-circular-progress__circle-clipper","mdc-circular-progress__circle-left"],[3,"ngTemplateOutlet"],[1,"mdc-circular-progress__gap-patch"],[1,"mdc-circular-progress__circle-clipper","mdc-circular-progress__circle-right"],["xmlns","http://www.w3.org/2000/svg","focusable","false",1,"mdc-circular-progress__indeterminate-circle-graphic"],["cx","50%","cy","50%"]],template:function(t,n){if(t&1&&(P(0,K,2,8,"ng-template",null,0,z),a(2,"div",2,1),y(),a(4,"svg",3),d(5,"circle",4),o()(),S(),a(6,"div",5)(7,"div",6)(8,"div",7),u(9,8),o(),a(10,"div",9),u(11,8),o(),a(12,"div",10),u(13,8),o()()()),t&2){let c=R(1);i(4),s("viewBox",n._viewBox()),i(),_("stroke-dasharray",n._strokeCircumference(),"px")("stroke-dashoffset",n._strokeDashOffset(),"px")("stroke-width",n._circleStrokeWidth(),"%"),s("r",n._circleRadius()),i(4),m("ngTemplateOutlet",c),i(2),m("ngTemplateOutlet",c),i(2),m("ngTemplateOutlet",c)}},dependencies:[L],styles:[`.mat-mdc-progress-spinner {
+  --mat-progress-spinner-animation-multiplier: 1;
+  display: block;
+  overflow: hidden;
+  line-height: 0;
+  position: relative;
+  direction: ltr;
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.6, 1);
+}
+.mat-mdc-progress-spinner circle {
+  stroke-width: var(--mat-progress-spinner-active-indicator-width, 4px);
+}
+.mat-mdc-progress-spinner._mat-animation-noopable, .mat-mdc-progress-spinner._mat-animation-noopable .mdc-circular-progress__determinate-circle {
+  transition: none !important;
+}
+.mat-mdc-progress-spinner._mat-animation-noopable .mdc-circular-progress__indeterminate-circle-graphic,
+.mat-mdc-progress-spinner._mat-animation-noopable .mdc-circular-progress__spinner-layer,
+.mat-mdc-progress-spinner._mat-animation-noopable .mdc-circular-progress__indeterminate-container {
+  animation: none !important;
+}
+.mat-mdc-progress-spinner._mat-animation-noopable .mdc-circular-progress__indeterminate-container circle {
+  stroke-dasharray: 0 !important;
+}
+@media (forced-colors: active) {
+  .mat-mdc-progress-spinner .mdc-circular-progress__indeterminate-circle-graphic,
+  .mat-mdc-progress-spinner .mdc-circular-progress__determinate-circle {
+    stroke: currentColor;
+    stroke: CanvasText;
+  }
+}
+
+.mat-progress-spinner-reduced-motion {
+  --mat-progress-spinner-animation-multiplier: 1.25;
+}
+
+.mdc-circular-progress__determinate-container,
+.mdc-circular-progress__indeterminate-circle-graphic,
+.mdc-circular-progress__indeterminate-container,
+.mdc-circular-progress__spinner-layer {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.mdc-circular-progress__determinate-container {
+  transform: rotate(-90deg);
+}
+.mdc-circular-progress--indeterminate .mdc-circular-progress__determinate-container {
+  opacity: 0;
+}
+
+.mdc-circular-progress__indeterminate-container {
+  font-size: 0;
+  letter-spacing: 0;
+  white-space: nowrap;
+  opacity: 0;
+}
+.mdc-circular-progress--indeterminate .mdc-circular-progress__indeterminate-container {
+  opacity: 1;
+  animation: mdc-circular-progress-container-rotate calc(1568.2352941176ms * var(--mat-progress-spinner-animation-multiplier)) linear infinite;
+}
+
+.mdc-circular-progress__determinate-circle-graphic,
+.mdc-circular-progress__indeterminate-circle-graphic {
+  fill: transparent;
+}
+
+.mat-mdc-progress-spinner .mdc-circular-progress__determinate-circle,
+.mat-mdc-progress-spinner .mdc-circular-progress__indeterminate-circle-graphic {
+  stroke: var(--mat-progress-spinner-active-indicator-color, var(--mat-sys-primary));
+}
+@media (forced-colors: active) {
+  .mat-mdc-progress-spinner .mdc-circular-progress__determinate-circle,
+  .mat-mdc-progress-spinner .mdc-circular-progress__indeterminate-circle-graphic {
+    stroke: CanvasText;
+  }
+}
+
+.mdc-circular-progress__determinate-circle {
+  transition: stroke-dashoffset 500ms cubic-bezier(0, 0, 0.2, 1);
+}
+
+.mdc-circular-progress__gap-patch {
+  position: absolute;
+  top: 0;
+  left: 47.5%;
+  box-sizing: border-box;
+  width: 5%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.mdc-circular-progress__gap-patch .mdc-circular-progress__indeterminate-circle-graphic {
+  left: -900%;
+  width: 2000%;
+  transform: rotate(180deg);
+}
+.mdc-circular-progress__circle-clipper .mdc-circular-progress__indeterminate-circle-graphic {
+  width: 200%;
+}
+.mdc-circular-progress__circle-right .mdc-circular-progress__indeterminate-circle-graphic {
+  left: -100%;
+}
+.mdc-circular-progress--indeterminate .mdc-circular-progress__circle-left .mdc-circular-progress__indeterminate-circle-graphic {
+  animation: mdc-circular-progress-left-spin calc(1333ms * var(--mat-progress-spinner-animation-multiplier)) cubic-bezier(0.4, 0, 0.2, 1) infinite both;
+}
+.mdc-circular-progress--indeterminate .mdc-circular-progress__circle-right .mdc-circular-progress__indeterminate-circle-graphic {
+  animation: mdc-circular-progress-right-spin calc(1333ms * var(--mat-progress-spinner-animation-multiplier)) cubic-bezier(0.4, 0, 0.2, 1) infinite both;
+}
+
+.mdc-circular-progress__circle-clipper {
+  display: inline-flex;
+  position: relative;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.mdc-circular-progress--indeterminate .mdc-circular-progress__spinner-layer {
+  animation: mdc-circular-progress-spinner-layer-rotate calc(5332ms * var(--mat-progress-spinner-animation-multiplier)) cubic-bezier(0.4, 0, 0.2, 1) infinite both;
+}
+
+@keyframes mdc-circular-progress-container-rotate {
+  to {
+    transform: rotate(360deg);
+  }
+}
+@keyframes mdc-circular-progress-spinner-layer-rotate {
+  12.5% {
+    transform: rotate(135deg);
+  }
+  25% {
+    transform: rotate(270deg);
+  }
+  37.5% {
+    transform: rotate(405deg);
+  }
+  50% {
+    transform: rotate(540deg);
+  }
+  62.5% {
+    transform: rotate(675deg);
+  }
+  75% {
+    transform: rotate(810deg);
+  }
+  87.5% {
+    transform: rotate(945deg);
+  }
+  100% {
+    transform: rotate(1080deg);
+  }
+}
+@keyframes mdc-circular-progress-left-spin {
+  from {
+    transform: rotate(265deg);
+  }
+  50% {
+    transform: rotate(130deg);
+  }
+  to {
+    transform: rotate(265deg);
+  }
+}
+@keyframes mdc-circular-progress-right-spin {
+  from {
+    transform: rotate(-265deg);
+  }
+  50% {
+    transform: rotate(-130deg);
+  }
+  to {
+    transform: rotate(-265deg);
+  }
+}
+`],encapsulation:2,changeDetection:0})}return r})();var G=(()=>{class r{static \u0275fac=function(t){return new(t||r)};static \u0275mod=M({type:r});static \u0275inj=x({imports:[F]})}return r})();var X=["*"];function Y(r,p){if(r&1&&(a(0,"span",2),B(1),o()),r&2){let e=l(2);i(),I(e.message())}}function ee(r,p){if(r&1&&(a(0,"div",0),d(1,"mat-progress-spinner",1),b(2,Y,2,1,"span",2),o()),r&2){let e=l();s("aria-label",e.message()||"\u0417\u0430\u0432\u0430\u043D\u0442\u0430\u0436\u0435\u043D\u043D\u044F"),i(),m("diameter",e.diameter()),i(),k(e.message()?2:-1)}}var H=class r{loading=f.required();message=f("");diameter=f(36);static \u0275fac=function(e){return new(e||r)};static \u0275cmp=g({type:r,selectors:[["app-section-loader"]],inputs:{loading:[1,"loading"],message:[1,"message"],diameter:[1,"diameter"]},ngContentSelectors:X,decls:2,vars:1,consts:[["role","status","aria-live","polite",1,"section-loader"],["mode","indeterminate",3,"diameter"],[1,"section-loader__message"]],template:function(e,t){e&1&&(D(),T(0),b(1,ee,3,3,"div",0)),e&2&&(i(),k(t.loading()?1:-1))},dependencies:[G,Q],styles:["[_nghost-%COMP%]{position:relative;display:flex;flex-direction:column;min-height:0;flex:1 1 auto}.section-loader[_ngcontent-%COMP%]{position:absolute;inset:0;z-index:2;display:flex;flex-direction:column;align-items:center;justify-content:center;gap:8px;border-radius:var(--section-loader-radius, var(--mv-radius));background:color-mix(in srgb,var(--mat-sys-surface) 66%,transparent);backdrop-filter:blur(2px);-webkit-backdrop-filter:blur(2px);animation:_ngcontent-%COMP%_section-loader-in .18s ease .15s both}.section-loader__message[_ngcontent-%COMP%]{padding:0 8px;color:var(--mat-sys-on-surface-variant);font-size:var(--mv-text-base);text-align:center}@keyframes _ngcontent-%COMP%_section-loader-in{0%{opacity:0}to{opacity:1}}@media(prefers-reduced-motion:reduce){.section-loader[_ngcontent-%COMP%]{animation-duration:.01ms}}"],changeDetection:0})};export{Q as a,G as b,H as c};
